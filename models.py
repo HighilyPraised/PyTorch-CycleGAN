10b8aea5fddf72c89e52ca2b53fef64410c4aa55
Yu <1319996259@qq.com>
2024-05-12 22:47:09 +0800

    Update models.py

--- models.py
+++ models.py
@@ -4,7 +4,6 @@ import torch.nn.functional as F
 class ResidualBlock(nn.Module):
     def __init__(self, in_features):
         super(ResidualBlock, self).__init__()
-
         conv_block = [  nn.ReflectionPad2d(1),
                         nn.Conv2d(in_features, in_features, 3),
                         nn.InstanceNorm2d(in_features),
@@ -12,9 +11,7 @@ class ResidualBlock(nn.Module):
                         nn.ReflectionPad2d(1),
                         nn.Conv2d(in_features, in_features, 3),
                         nn.InstanceNorm2d(in_features)  ]
-
         self.conv_block = nn.Sequential(*conv_block)
-
     def forward(self, x):
         return x + self.conv_block(x)
 
@@ -32,7 +29,8 @@ class Generator(nn.Module):
         in_features = 64
         out_features = in_features*2
         for _ in range(2):
-            model += [  nn.Conv2d(in_features, out_features, 3, stride=2, padding=1),
+            model += [  nn.Conv2d(in_features, out_features, 3,
+                                  stride=2, padding=1),
                         nn.InstanceNorm2d(out_features),
                         nn.ReLU(inplace=True) ]
             in_features = out_features
@@ -45,7 +43,9 @@ class Generator(nn.Module):
         # Upsampling
         out_features = in_features//2
         for _ in range(2):
-            model += [  nn.ConvTranspose2d(in_features, out_features, 3, stride=2, padding=1, output_padding=1),
+            model += [  nn.ConvTranspose2d(
+                            in_features, out_features, 3,
+                            stride=2, padding=1, output_padding=1),
                         nn.InstanceNorm2d(out_features),
                         nn.ReLU(inplace=True) ]
             in_features = out_features
@@ -64,26 +64,20 @@ class Generator(nn.Module):
 class Discriminator(nn.Module):
     def __init__(self, input_nc):
         super(Discriminator, self).__init__()
-
-        # A bunch of convolutions one after another
         model = [   nn.Conv2d(input_nc, 64, 4, stride=2, padding=1),
                     nn.LeakyReLU(0.2, inplace=True) ]
-
         model += [  nn.Conv2d(64, 128, 4, stride=2, padding=1),
                     nn.InstanceNorm2d(128), 
                     nn.LeakyReLU(0.2, inplace=True) ]
-
         model += [  nn.Conv2d(128, 256, 4, stride=2, padding=1),
                     nn.InstanceNorm2d(256), 
                     nn.LeakyReLU(0.2, inplace=True) ]
-
         model += [  nn.Conv2d(256, 512, 4, padding=1),
                     nn.InstanceNorm2d(512), 
                     nn.LeakyReLU(0.2, inplace=True) ]
 
         # FCN classification layer
         model += [nn.Conv2d(512, 1, 4, padding=1)]
-
         self.model = nn.Sequential(*model)
 
     def forward(self, x):
